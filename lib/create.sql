0ee292611a5769aacc486793d8e439c8d0020c2e
Kevin Choi <kevinnwjchoi@gmail.com>
2024-05-20 17:59:19 -0400

    complete

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id INTEGER, 
+    name TEXT,
+    age INTGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive INTEGER
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5 ,'M' ,'Brown', 'Calm', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'Black', 'Crazy' , 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'Brown', 'Calm' , 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'M', 'Purple', 'Sane', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 2, 'F', 'Gray', 'Sassy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 25, 'M', 'Green', 'Crazy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'F', 'Red', 'Calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 6, 'M', 'Black', NULL, 1);
+
